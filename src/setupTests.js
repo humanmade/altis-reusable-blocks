f0b75b7763cd10362c1cf6a11949b0149d2a9ddf
Kevin Langley <me@kevinlangleyjr.com>
2020-02-11 13:03:02 -0500

    Stripping unused dev dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
-const React = require( 'react' );
+import React from 'react';
 
 global.wp = {
 	// The plugin @babel/plugin-transform-react-jsx is configured to use wp.element.createElement as pragma.
