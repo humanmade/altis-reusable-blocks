6a05515a17024db4ebaf778eadb9ae46f85fc15e
Robert O'Rourke <robert@o-rourke.org>
2022-12-13 12:32:46 +0000

    Fix schema for search endpoint
    Should be using a callback and not an array directly.

--- inc/rest-api/search/class-rest-endpoint.php
+++ inc/rest-api/search/class-rest-endpoint.php
@@ -52,7 +52,7 @@ class REST_Endpoint {
 			[
 				'methods'             => 'GET',
 				'callback'            => [ $this, 'get_items' ],
-				'schema'              => ( new WP_REST_Blocks_Controller( 'wp_block' ) )->get_item_schema(),
+				'schema'              => [ new WP_REST_Blocks_Controller( 'wp_block' ), 'get_item_schema' ],
 				'permission_callback' => function() {
 					return current_user_can( 'read_wp_block' );
 				},
